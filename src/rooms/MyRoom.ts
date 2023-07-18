9b06de8180b95c90417ba7b777a5a6fdc438d518
권좌영 <kzoyoung@naver.com>
2023-07-19 01:10:23 +0900

    fix:이동로직수정

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -7,56 +7,43 @@ export class MyRoom extends Room<MyRoomState> {
   onCreate(options: any) {
     this.setState(new MyRoomState());
 
+    
+    
     // handle player input
     this.onMessage(0, (client, input) => {
       const player = this.state.players.get(client.sessionId);
-      const velocity = 1;
-      const halfVelocity = Math.sqrt(Math.pow(velocity, 2) / 2);
-      const speed = 200;
-
+  
+      this.onMessage(1, (client, message: {xc:number,yc:number }) => {
+        player.x = message.xc;
+        player.y = message.yc;
+       
+      });
       if (input.left && input.up) {
-        player.x -= halfVelocity;
-        player.y -= halfVelocity;
-        player.velX = speed;
-        player.velY = speed;
+
         player.animeState = "idle_left";
       } else if (input.right && input.up) {
-        player.x += halfVelocity;
-        player.y -= halfVelocity;
-        player.velX = speed;
-        player.velY = speed;
+
         player.animeState = "idle_right";
       } else if (input.left && input.down) {
-        player.x -= halfVelocity;
-        player.y += halfVelocity;
-        player.velX = speed;
-        player.velY = speed;
+
         player.animeState = "idle_left";
       } else if (input.right && input.down) {
-        player.x += halfVelocity;
-        player.y += halfVelocity;
-        player.velX = speed;
-        player.velY = speed;
+
         player.animeState = "idle_right";
       } else if (input.left) {
-        player.x -= velocity;
-        player.velX = speed;
+
         player.animeState = "idle_left";
       } else if (input.right) {
-        player.x += velocity;
-        player.velX = speed;
+
         player.animeState = "idle_right";
       } else if (input.up) {
-        player.y -= velocity;
-        player.velY = speed;
+
         player.animeState = "idle_up";
       } else if (input.down) {
-        player.y += velocity;
-        player.velY = speed;
+
         player.animeState = "idle_down";
       } else {
-        player.velX = 0;
-        player.velY = 0;
+
       }
     });
 
