be4d126986de47a637b33242bacbc4e447814b80
권좌영 <kzoyoung@naver.com>
2023-08-20 13:40:11 +0900

    feat:커스텀아이디설정

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -7,12 +7,19 @@ export class MyRoom extends Room<MyRoomState> {
   onCreate(options: any) {
     this.setState(new MyRoomState());
 
+    this.onMessage(2, (client, message: {name:String }) => {
+      const player = this.state.players.get(client.sessionId);
+
+      player.name = message.name;
+      console.log(player.name);
+    });
+
     this.onMessage(1, (client, message: {xc:number,yc:number }) => {//플레이어의 좌표정보를 받고 저장
       const player = this.state.players.get(client.sessionId);
 
       player.x = message.xc;      //player.x player.y의 값이 바뀌면서 클라이언트의 player.onChange부분이 실행되면서 클라이언트로 좌표정보가 전달됨
       player.y = message.yc;
-     
+      
     });
     
     
@@ -52,24 +59,29 @@ export class MyRoom extends Room<MyRoomState> {
     // handle chat messages
     this.onMessage("chat", (client, message) => {
       console.log(client.sessionId, "said:", message);
-
+      const player = this.state.players.get(client.sessionId);
       // broadcast chat message to all clients in the room
-      this.broadcast("chat", { id: client.sessionId, message: message });
+      this.broadcast("chat", { id: player.name, message: message });
     });
   }
 
   onJoin(client: Client, options: any) {
+    
     console.log(client.sessionId, "joined!");
+    
+
 
     const mapWidth = 800;
     const mapHeight = 600;
 
     const player = new Player();
-
+    
     player.x = 650;
     player.y = 430;
-
+    
+ 
     this.state.players.set(client.sessionId, player);
+    
   }
 
   onLeave(client: Client, consented: boolean) {
