6d9f02ad7b3b745a443b6fe0446feaaeaf427d7f
Pyung <pyung1591@gmail.com>
2023-08-21 09:55:02 +0900

    fix : 시작위치 조정

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -7,52 +7,42 @@ export class MyRoom extends Room<MyRoomState> {
   onCreate(options: any) {
     this.setState(new MyRoomState());
 
-    this.onMessage(2, (client, message: {name:String }) => {
+    this.onMessage(2, (client, message: { name: String }) => {
       const player = this.state.players.get(client.sessionId);
 
       player.name = message.name;
       console.log(player.name);
     });
 
-    this.onMessage(1, (client, message: {xc:number,yc:number }) => {//플레이어의 좌표정보를 받고 저장
+    this.onMessage(1, (client, message: { xc: number; yc: number }) => {
+      //플레이어의 좌표정보를 받고 저장
       const player = this.state.players.get(client.sessionId);
 
-      player.x = message.xc;      //player.x player.y의 값이 바뀌면서 클라이언트의 player.onChange부분이 실행되면서 클라이언트로 좌표정보가 전달됨
+      player.x = message.xc; //player.x player.y의 값이 바뀌면서 클라이언트의 player.onChange부분이 실행되면서 클라이언트로 좌표정보가 전달됨
       player.y = message.yc;
-      
     });
-    
-    
+
     // handle player input
     this.onMessage(0, (client, input) => {
       const player = this.state.players.get(client.sessionId);
 
       if (input.left && input.up) {
-
         player.animeState = "idle_left";
       } else if (input.right && input.up) {
-
         player.animeState = "idle_right";
       } else if (input.left && input.down) {
-
         player.animeState = "idle_left";
       } else if (input.right && input.down) {
-
         player.animeState = "idle_right";
       } else if (input.left) {
-
         player.animeState = "idle_left";
       } else if (input.right) {
-
         player.animeState = "idle_right";
       } else if (input.up) {
-
         player.animeState = "idle_up";
       } else if (input.down) {
-
         player.animeState = "idle_down";
       } else {
-
       }
     });
 
@@ -66,22 +56,17 @@ export class MyRoom extends Room<MyRoomState> {
   }
 
   onJoin(client: Client, options: any) {
-    
     console.log(client.sessionId, "joined!");
-    
-
 
     const mapWidth = 800;
     const mapHeight = 600;
 
     const player = new Player();
-    
-    player.x = 650;
-    player.y = 430;
-    
- 
+
+    player.x = 1050;
+    player.y = 830;
+
     this.state.players.set(client.sessionId, player);
-    
   }
 
   onLeave(client: Client, consented: boolean) {
